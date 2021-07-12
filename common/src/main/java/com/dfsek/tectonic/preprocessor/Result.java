6acf40423a1c487d355e7d5b85ad3bcf0dfb9546
dfsek <dfsek@protonmail.com>
2021-07-12 10:08:35 -0700

    finish implementing Result

--- common/src/main/java/com/dfsek/tectonic/preprocessor/Result.java
+++ common/src/main/java/com/dfsek/tectonic/preprocessor/Result.java
@@ -1,33 +1,53 @@
 package com.dfsek.tectonic.preprocessor;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class Result<T> {
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
+public class Result<T> implements UnaryOperator<T> {
     private final TransformType type;
     private final T overwritten;
-    private final T transformed;
+    private final Consumer<T> transformer;
 
-    private Result(@Nullable T overwritten, @Nullable T transformed, TransformType type) {
+    private Result(T overwritten, Consumer<T> transformer, TransformType type) {
         this.type = type;
         this.overwritten = overwritten;
-        this.transformed = transformed;
+        this.transformer = transformer;
     }
 
     public static <T> Result<T> overwrite(T result) {
         return new Result<>(result, null, TransformType.OVERWRITE);
     }
 
-    public static <T> Result<T> fail() {
-        return new Result<>(null, null, TransformType.FAIL);
+    public static <T> Result<T> noOp() {
+        return new Result<>(null, null, TransformType.NOP);
     }
 
-    public static <T> Result<T> transform(T transformed) {
-        return new Result<>(null, transformed, TransformType.TRANSFORM);
+    public static <T> Result<T> transform(@NotNull Consumer<T> transformer) {
+        Objects.requireNonNull(transformer);
+        return new Result<>(null, transformer, TransformType.TRANSFORM);
     }
 
-    protected enum TransformType {
+    @Override
+    public T apply(T t) {
+        switch(type) {
+            case NOP:
+                return t;
+            case OVERWRITE:
+                return overwritten;
+            case TRANSFORM:
+                transformer.accept(t);
+                return t;
+        }
+        return t;
+    }
+
+    private enum TransformType {
         OVERWRITE,
         TRANSFORM,
-        FAIL
+        NOP
     }
 }
