b5b145c2274b2d049fd1fdc3d17e8cc6ac6c8973
dfsek <dfsek@protonmail.com>
2021-07-11 21:58:45 -0700

    make exceptions unchecked

--- src/main/java/com/dfsek/tectonic/exception/ConfigException.java
+++ src/main/java/com/dfsek/tectonic/exception/ConfigException.java
@@ -1,6 +1,6 @@
 package com.dfsek.tectonic.exception;
 
-public abstract class ConfigException extends Exception {
+public abstract class ConfigException extends RuntimeException {
     private static final long serialVersionUID = 1316896835740257026L;
 
     public ConfigException(String message) {
