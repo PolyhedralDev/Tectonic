3b244e0adaae9a23f28bfed4297eae14b9ec87a7
dfsek <dfsek@protonmail.com>
2021-07-12 07:30:46 -0700

    cleanup JsonConfiguration

--- lang/json/src/main/java/com/dfsek/tectonic/json/JsonConfiguration.java
+++ lang/json/src/main/java/com/dfsek/tectonic/json/JsonConfiguration.java
@@ -2,14 +2,12 @@ package com.dfsek.tectonic.json;
 
 import com.dfsek.tectonic.config.Configuration;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Map;
 
-public class JsonConfiguration  implements Configuration {
+public class JsonConfiguration implements Configuration {
     private final Object config;
 
     private final String name;
