1158fe55e9f4de5c4f9dd1f8d276bce59bcd4e76
dfsek <dfsek@protonmail.com>
2021-12-13 15:30:51 -0700

    update dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -22,16 +22,16 @@ tasks.test {
 }
 
 dependencies {
-    compileOnly("org.jetbrains:annotations:20.1.0")
-    implementation("commons-io:commons-io:2.8.0")
+    compileOnly("org.jetbrains:annotations:22.0.0")
+    implementation("commons-io:commons-io:2.11.0")
 
     // JUnit.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
-    classifier = "sources"
+    archiveClassifier.set("sources")
     from(sourceSets.main.get().allSource)
 }
 
--- lang/yaml/build.gradle.kts
+++ lang/yaml/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
-    classifier = "sources"
+    archiveClassifier.set("sources")
     from(sourceSets.main.get().allSource)
 }
 
