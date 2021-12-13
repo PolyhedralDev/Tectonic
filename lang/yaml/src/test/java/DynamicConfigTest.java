137b4cb9f6231bbca3fac54e165054f60ec7c6a2
dfsek <dfsek@protonmail.com>
2021-12-13 15:18:48 -0700

    add test for dynamic config default values

--- lang/yaml/src/test/java/DynamicConfigTest.java
+++ lang/yaml/src/test/java/DynamicConfigTest.java
@@ -4,6 +4,8 @@ import com.dfsek.tectonic.api.loader.ConfigLoader;
 import com.dfsek.tectonic.yaml.YamlConfiguration;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DynamicConfigTest {
     @Test
     public void testDynamicConfig() {
@@ -17,12 +19,18 @@ public class DynamicConfigTest {
                         .builder("number", double.class)
                         .setFinal()
                         .build())
+                .value("default", DynamicValue
+                        .builder("nonexistent-value", int.class)
+                        .setFinal()
+                        .setDefault(5)
+                        .build())
                 .build();
 
         ConfigLoader loader = new ConfigLoader();
         loader.load(template, new YamlConfiguration(TypeTest.class.getResourceAsStream("/test.yml")));
 
-        System.out.println(template.get("val2", double.class));
-        System.out.println(template.get("val1", String.class));
+        assertEquals(template.get("val2", double.class), 3.0);
+        assertEquals(template.get("val1", String.class), "test!");
+        assertEquals(template.get("default", int.class), 5);
     }
 }
