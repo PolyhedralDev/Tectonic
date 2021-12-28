6861a96937d75724ac6f114b6ffcf87f7e741ded
dfsek <dfsek@protonmail.com>
2021-12-27 20:28:20 -0700

    make other levels package-private

--- common/src/main/java/com/dfsek/tectonic/api/depth/EntryLevel.java
+++ common/src/main/java/com/dfsek/tectonic/api/depth/EntryLevel.java
@@ -3,7 +3,7 @@ package com.dfsek.tectonic.api.depth;
 /**
  * An entry in a map.
  */
-public class EntryLevel implements Level {
+class EntryLevel implements Level {
     private final String name;
 
     public EntryLevel(String name) {
